83368da9b801f12f9c5c551805a4091a07878c2e
mariya-polyakova1991 <mariyaluk@yandex.ru>
2020-08-05 11:47:13 +0300

    fix form

--- js/form.js
+++ js/form.js
@@ -1,7 +1,10 @@
 var modals = document.getElementById("modals");
 const openModalsButton = document.getElementById("buttons-open");
 var closeModalsButton = modals.querySelector(".buttons-close");
-const commentList = document.getElementById("comments-list");
+const commentList = document.getElementById("comments-name");
+const commentEmail = document.getElementById("comments-email");
+const commentMessage = document.getElementById("comments-message");
+
 const commentField = document.getElementById("comment-field");
 const name = document.getElementById("login-field");
 const email = document.getElementById("email-field");
@@ -9,17 +12,23 @@ const package = document.getElementById("package-field");
 
 openModalsButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modals.classList.add("modals--show");
-  console.log("hello");
   commentList.classList.add("comments-list");
-  commentList.textContent =
-    "Your name: " +
-    name.value +
-    ", Your email: " +
-    email.value +
-    ", Your message: " +
-    commentField.value;
+  commentEmail.classList.add("comments-list");
+  commentMessage.classList.add("comments-list");
 
+  modals.classList.add("modals--show");
+  // const textName = document.createTextNode(Your name:  ${name.value},);
+  // commentList.append(textName);
+  // const textEmail = document.createTextNode(Your email: ${email.value},);
+  // commentEmail.append(textEmail);
+  // const textMessage = document.createTextNode(Your message: ${commentField.value});
+  // commentMessage.append(textMessage);
+  commentList.textContent = `
+  Your name:  ${name.value} `;
+  commentEmail.textContent = `
+  Your email: ${email.value} `;
+  commentMessage.textContent = `
+  Your message: ${commentField.value}`;
   name.value = "";
   email.value = "";
   commentField.value = "";
